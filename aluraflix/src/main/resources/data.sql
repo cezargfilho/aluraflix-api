78ea6ece831f9a4c4caecfcef8c020e995933cec
Cezar Gonzalez Filho <cezar.gfilho@gmail.com>
2021-08-02 19:33:42 -0300

    Update data.sql

--- aluraflix/src/main/resources/data.sql
+++ aluraflix/src/main/resources/data.sql
@@ -5,8 +5,8 @@ INSERT INTO CATEGORY(title, color) VALUES('DATA SCIENCE','#FFF700');
 INSERT INTO CATEGORY(title, color) VALUES('DESIGN','#0049FF');
 
 INSERT INTO VIDEO(title, description, url, category_id) VALUES('React casa 50 MILHÕES', 'React de casas milionarias', 'https://youtu.be/p31HeB2_7eI', 2);
-INSERT INTO VIDEO(title, description, url, category_id) VALUES('CASIMIRO REAGE À RESPOSTA DE MARCELO CABO EM COLETIVA APÓS EMPATE COM O NÁUTICO | Cortes do Casimito', 'Live do dia 18/07/2021 para o dia 19/07/2021', 'https://youtu.be/0AylNJxkjCg', 3);
-INSERT INTO VIDEO(title, description, url, category_id) VALUES('CASIMIRO VAI A LOUCURA COM REGRA DA CBF E DESAFIA ROGÉRIO CENI A SALVAR O VASCO | Cortes do Casimito', 'Live do dia 18/07/2021 para o dia 19/07/2021', 'https://youtu.be/GgLPAmbiLkE', 4);
+INSERT INTO VIDEO(title, description, url, category_id) VALUES('CASIMIRO REAGE À RESPOSTA DE MARCELO | Cortes do Casimito', 'Live do dia 18/07/2021 para o dia 19/07/2021', 'https://youtu.be/0AylNJxkjCg', 3);
+INSERT INTO VIDEO(title, description, url, category_id) VALUES('CASIMIRO VAI A LOUCURA COM REGRA DA CBF | Cortes do Casimito', 'Live do dia 18/07/2021 para o dia 19/07/2021', 'https://youtu.be/GgLPAmbiLkE', 4);
 INSERT INTO VIDEO(title, description, url, category_id) VALUES('QUANTAS HORAS UM ADULTO PRECISA DORMIR POR DIA? | Cortes do Casimito', 'Live do dia 14/07/2021 para o dia 15/07/2021', 'https://youtu.be/5ZngG4Rcx8U', 4);
 
 INSERT INTO VIDEO(title, description, url, category_id) VALUES('GOLS DA RODADA BRASILEIRAO', 'Todos os gols da rodada do brasileirao 2021', 'http://youtube.com', 1);
