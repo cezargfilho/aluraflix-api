b3afc16cdad1a10845a2b057f036f1d6f22a85f7
Cezar Gonzalez Filho <cezar.gfilho@gmail.com>
2021-08-24 17:56:24 -0300

    Refatoração de testes de integração e adaptação de classes

--- aluraflix/src/main/java/br/com/alura/aluraflix/controller/form/VideoForm.java
+++ aluraflix/src/main/java/br/com/alura/aluraflix/controller/form/VideoForm.java
@@ -5,7 +5,14 @@ import java.util.Optional;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import br.com.alura.aluraflix.config.validation.NotBlankConstraint;
 import br.com.alura.aluraflix.exception.EntityNotFoundException;
 import br.com.alura.aluraflix.exception.ExceptionMessages;
 import br.com.alura.aluraflix.model.Category;
@@ -17,17 +24,40 @@ public class VideoForm {
 
 	@NotBlank
 	@Length(min = 10, max = 100)
+	@JsonProperty(required = true)
 	private String title;
 
 	@NotBlank
 	@Length(min = 10, max = 50)
+	@JsonProperty(required = true)
 	private String description;
 
 	@NotBlank
 	@Length(min = 10, max = 60)
+	@JsonProperty(required = true)
 	private String url;
 
+	@NotBlankConstraint
+	@NumberFormat(style = Style.NUMBER)
+	@JsonInclude(value = Include.NON_NULL)
+	@JsonProperty(required = false)
 	private String categoryId;
+	
+	@Deprecated
+	public VideoForm() {}
+
+	public VideoForm(String title, String description, String url, String categoryId) {
+		this.title = title;
+		this.description = description;
+		this.url = url;
+		this.categoryId = categoryId;
+	}
+
+	public VideoForm(String title, String description, String url) {
+		this.title = title;
+		this.description = description;
+		this.url = url;
+	}
 
 	public String getTitle() {
 		return title;
